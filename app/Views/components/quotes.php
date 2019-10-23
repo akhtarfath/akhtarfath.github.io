fd2ae8d38c8cc2bd0b63d701b2b5fe2e9fb8efd6
Muhammad A. Fathan <muhammadfathan23@gmail.com>
2019-10-23 13:39:19 +0700

    git eaa

---
+++ app/Views/components/quotes.php
@@ -0,0 +1,55 @@
+
+<div id="colorlib-testimony">
+	<div class="container">
+		<div class="row text-center">
+			<h2 class="bold"> Awesome </h2>
+		</div>
+		<div class="row">
+			<div class="col-md-12 col-md-offset-0 text-center animate-box intro-heading">
+				<span> Qur'an </span>
+				<h2 class="oren head-text"> Quotes </h2>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-md-12">
+				<div class="rotate">
+					<h2 class="heading">Says</h2>
+				</div>
+			</div>
+		</div>
+		<div class="row animate-box">
+			<div class="owl-carousel">
+				<!-- <div class="item">
+					<div class="col-md-12 text-center">
+						<div class="testimony">
+							<blockquote>
+								<p>"Leave what you must, abandon who you must, sacrifice whatever you must - just to preserve yout faith and avoid fitna in your din"</p>
+								<span>" &mdash; Surah Al-Kahf </span>
+							</blockquote>
+						</div>
+					</div>
+				</div> -->
+				<div class="item">
+					<div class="col-md-12 text-center">
+						<div class="testimony">
+							<blockquote>
+								<p>"He will give you [something] better than what was taken form you"</p>
+								<span>" &mdash; Qur'an 8:70 </span>
+							</blockquote>
+						</div>
+					</div>
+				</div>
+				<div class="item">
+					<div class="col-md-12 text-center">
+						<div class="testimony">
+							<blockquote>
+								<p>"Do not grieve, indeed Allah is with us"</p>
+								<span>" &mdash; Qur'an 9:40 </span>
+							</blockquote>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
