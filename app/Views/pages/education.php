f2732f0e7da2ee4fd5f59ec5e2435336c5d1bea3
Muhammad A. Fathan <muhammadfathan23@gmail.com>
2019-10-23 13:09:19 +0700

    hahaha

--- app/Views/pages/education.php
+++
@@ -1,8 +0,0 @@
-<div id="colorlib-page">
-	<?php
-		echo view('components/navbar');
-		echo view('components/education');
-		echo view('components/footer');
-	?>
-</div>
-<?php echo view('style/style'); echo view('style/style2'); ?>
--- app/Views/pages/portfolio.php
+++
@@ -1,8 +0,0 @@
-<div id="colorlib-page">
-	<?php
-		echo view('components/navbar');
-		echo view('components/portfolio');
-		echo view('components/footer');
-	?>
-</div>
-<?php echo view('style/style'); ?>
